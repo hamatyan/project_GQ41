d7a4938c24c0b21d3d1c819c01f5ff0d8027dfd0
hamanyan <hiroakipokemon@gmail.com>
2021-08-29 18:30:47 +0900

    これでいちようステージ作成はできる

--- retoroGame/Assets/_retoroGame/Script/Stage/CreateStage.cs
+++ retoroGame/Assets/_retoroGame/Script/Stage/CreateStage.cs
@@ -10,8 +10,21 @@ namespace _retoroGame.Stage
 {
 	public class CreateStage : MonoBehaviour
 	{
+		//ステージ生成列挙型
+		public enum CreateStageEnum
+		{
+			BorderlineWall,
+			Wall,
+			GoalWall,
+		}
+		public CreateStageEnum createstageEnum;
+
 		public StageManager stageManager;  //ステージ格納用
 		public int stage_obj_cnt;
+
+		//これは壁の生成に使う変数
+		public Transform startPos;
+		public Transform goalPos;
 
 		private void Awake()
 		{
@@ -21,13 +34,44 @@ namespace _retoroGame.Stage
 		// Start is called before the first frame update
 		void Start()
 		{
+			switch(createstageEnum)
+			{
+				case CreateStageEnum.BorderlineWall:
+					CreateBorderlineWall();
+					break;
+				case CreateStageEnum.Wall:
+					float p = 0;
+					if (startPos.position.x == goalPos.position.x)
+					{
+						p = startPos.position.z - goalPos.position.z;
+						startPos.transform.DOScaleZ(p, 1f).SetEase(Ease.Linear);
+					}
+					if (startPos.position.z == goalPos.position.z)
+					{
+						p = startPos.position.x - goalPos.position.x;
+						startPos.transform.DOScaleX(p, 1f).SetEase(Ease.Linear);
+					}
+					break;
+				case CreateStageEnum.GoalWall:
+					break;
+			}
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+
+		void CreateBorderlineWall()
+		{
 			float p = 0;
 			//番号を順番につなげる(本当はエディタ上で動作したい)
 			for (int i = 0; i < stage_obj_cnt - 1; i++)
 			{
-				if(stageManager.stageObj[i].transform.position.x == stageManager.stageObj[i + 1].transform.position.x)
+				if (stageManager.stageObj[i].transform.position.x == stageManager.stageObj[i + 1].transform.position.x)
 				{
-					p = stageManager.stageObj[i].transform.position.z - stageManager.stageObj[i+1].transform.position.z;
+					p = stageManager.stageObj[i].transform.position.z - stageManager.stageObj[i + 1].transform.position.z;
 					stageManager.stageObj[i].transform.DOScaleZ(p, 1f).SetEase(Ease.Linear);
 				}
 				if (stageManager.stageObj[i].transform.position.z == stageManager.stageObj[i + 1].transform.position.z)
@@ -38,12 +82,6 @@ namespace _retoroGame.Stage
 			}
 			p = stageManager.stageObj[0].transform.position.x - stageManager.stageObj[3].transform.position.x;
 			stageManager.stageObj[3].transform.DOScaleX(p, 1f).SetEase(Ease.Linear);
-		}
-
-		// Update is called once per frame
-		void Update()
-		{
-
 		}
 	}
 }
